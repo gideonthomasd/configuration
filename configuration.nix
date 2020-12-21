945a9d851c9255a9df023a181d7c37467603bc97
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2020-12-21 22:01:15 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -105,6 +105,9 @@ services.xserver.windowManager.openbox.enable = true;
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
+  
+  ##hardware.bluetooth.enable = true;
+  ##services.blueman.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
